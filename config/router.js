c5a61a4b8f10b204c84f5c4966294036c6fb0f34
jgjadaoag <jjadaoag1@up.edu.ph>
2015-11-22 16:53:04 +0800

    Fixed login and logout

--- config/router.js
+++ config/router.js
@@ -3,27 +3,6 @@ var passport = require('passport');
 var Strategy = require('passport-local').Strategy;
 var db = require('./../db');
 
-passport.use(new Strategy(
-  function(username, password, cb) {
-    db.users.findByUsername(username, function(err, user) {
-      if (err) { return cb(err); }
-      if (!user) { return cb(null, false); }
-      if (user.password != password) { return cb(null, false); }
-      return cb(null, user);
-    });
-  }));
-
-passport.serializeUser(function(user, cb) {
-  cb(null, user.studentno);
-});
-
-passport.deserializeUser(function(id, cb) {
-  db.users.findById(id, function (err, user) {
-    if (err) { return cb(err); }
-    cb(null, user);
-  });
-});
-
 
 
 module.exports = function(router){
@@ -34,7 +13,7 @@ module.exports = function(router){
 				res.redirect('/user/#/feed');
 			});
 
-	router.route('logout')
+	router.route('/logout')
 		.get(
 			 function(req, res){
    				 req.logout();
